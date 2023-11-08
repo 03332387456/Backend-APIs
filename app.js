b5100029477b4043b7911a5d2eafbd7a7f4f430e
03332387456 <sahirmavia@gmail.com>
2023-11-08 11:08:44 +0500

    APIs form backend

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors")
+const mongoose = require("mongoose");
+const courseRoute = require("./routes/courseroute");
+
+
+const App = express();
+App.use(cors())
+App.use(express.json());
+
+App.use("/courses", courseRoute);
+
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        App.listen(process.env.PORT, () => {
+            console.log(
+                `Database Connected and server is listening http://localhost:${process.env.PORT}`
+            );
+        });
+    })
+    .catch((err) => {
+        console.log("err", err);
+    });
